79a02d7a3dc02110d834998fd0d0b27e97feef2e
Matej-Sulfrian <matej.sulfrian@hs-furtwangen.de>
2023-11-21 19:12:12 +0100

    added comments on smileDetection.js and jokes.js

--- game/jokes/jokes.js
+++ game/jokes/jokes.js
@@ -1,15 +1,18 @@
+// call init function when page is loaded
 window.addEventListener("load", init);
 
+// define global variables
 let jokes;
 let nextBtn;
 let jokeCountEl;
 let jokeEl;
 let refreshLifeEl;
 
+// function called initial on page load
 function init () {
-
+	// get jokes from api
 	getJokes();
-
+	// get DOM Element references
 	nextBtn = document.querySelector(".next")
 	nextBtn.addEventListener("click", onNextBtnClick)
 
@@ -21,51 +24,56 @@ function init () {
 
 }
 
+// function to show next joke
 function onNextBtnClick () {
-
+	// check if still jokes available
 	if (jokes.length > 0) {
 		console.log("Next joke...")
-
+		// write joke to DOM
 		jokeEl.innerHTML = jokes[0].joke
+		// remove joke from que (array)
 		jokes.shift()
-
+		// update joke count
 		jokeCountEl.innerHTML = 10 - jokes.length
 	} else {
-
+		// get new joke from api
 		getJokes()
 	}
-
-
 }
 
+// function to get jokes from api
 function getJokes() {
-
+	// fetch jokes with api parameter in URL
 	 fetch('https://v2.jokeapi.dev/joke/Any?type=single&amount=10')
 		.then(response => {
 			if (!response.ok) {
 				throw new Error('Network response was not ok');
 			}
+			// return the response JSON object
 			return response.json();
 		})
 		.then(data => {
 			// handle the JSON data
-
 			jokes = data.jokes
-
+			// write first joke to DOM
 			jokeEl.innerHTML = jokes[0].joke
+			// remove first joke from DOM
 			jokes.shift()
-
+			// update jokes count
 			jokeCountEl.innerHTML = 10 - jokes.length
-
 		})
 		.catch(error => {
 			console.error('There was a problem with the fetch operation:', error);
 		});
 }
 
+// function to reset lives
 function refreshLife () {
+	// get all heart elements
 	let lifeEls = document.querySelectorAll(".icofont-heart")
+	// loop over the heart elements
 	for (let el of lifeEls) {
+		// add the class 'active' to the element (so the heart is red again)
 		el.classList.add('active')
 	}
 }
--- game/smile/smileDetection.js
+++ game/smile/smileDetection.js
@@ -1,19 +1,16 @@
 // DOM Elements references
 const video = document.getElementById("video");
 const startButton = document.querySelector("#start-button");
-const multi=document.querySelector("#multi");
+const multi = document.querySelector("#multi"); // Face tracking elements and stats
 
 // Smile detection variables
-const MIN_CONSECUTIVE_FRAMES = 3;
-let smileCounter1 = 0;
-let smileCounter2 = 0;
-let consecutiveSmiles1 = 0;
-let consecutiveSmiles2 = 0;
-let isSmiling1 = false;
-let isSmiling2 = false;
-let intervalStarted = false;
-let gameStarted = false;
+const MIN_CONSECUTIVE_FRAMES = 3;  // lenght of smiling (threshold) in frames
+let consecutiveSmiles1 = 0; // goes up each frame, when smile is detected
+let isSmiling1 = false; // is set to true wehen smiling to prevent loosing all lives at once
+let intervalStarted = false; // indicates whether a smile detection itteration is running atm
+let gameStarted = false; // indicates whether the game is started atm
 
+// Start/stop button function
 function onStartStop () {
 	if (startButton.innerHTML == "Stop") {
 		endGame()
@@ -24,30 +21,31 @@ function onStartStop () {
 	}
 }
 
-// Function to start the smile detection game
+// Function to start the smile detection
 function startGame() {
-	// Reset game state variables
-	smileCounter1 = 0;
-	smileCounter2 = 0;
+	// Give a visual feedback, the game has started
 	video.style.border="2px solid red";
+	// show the face tracking elements and stats
 	multi.style.display = 'flex';
-
 	// Indicate that the game has started
 	gameStarted = true;
 
 }
 
+// Function to stop the smile detection
 function endGame() {
-	// Reset game state variables
-	gameStarted = false;
-	multi.style.display = 'none';
+	// Give a visual feedback, the game has started
 	video.style.border="2px solid white";
+	// hide the face tracking elements and stats
+	multi.style.display = 'none';
+	// Reset game state variable
+	gameStarted = false;
 }
 
-
+// Function to remove a life
 function updateLife () {
-	let activeLifeEls = document.querySelectorAll(".icofont-heart.active")
-	activeLifeEls[0].classList.remove('active')
+	let activeLifeEls = document.querySelectorAll(".icofont-heart.active") // get all elements with class 'active' (red hearts)
+	activeLifeEls[0].classList.remove('active')// remove the class 'active' from the first element (so its not red anymore) -> see css
 }
 
 
@@ -77,22 +75,28 @@ async function startSmileDetection() {
 
 		// Process the video feed for smile detection
 		video.addEventListener("play", () => {
+			// Create canvas element from face tracking stats
 			const canvas = faceapi.createCanvasFromMedia(video);
+			// Append canvas
 			document.getElementById('multi').append(canvas);
 
 			// Set canvas position to absolute
 			canvas.style.position = 'absolute';
 
+			// Set dimenaions form canvas
 			const displaySize = { width: video.offsetWidth, height: video.offsetHeight };
 			faceapi.matchDimensions(canvas, displaySize);
 
 			// Setting zIndex to ensure canvas is above the video
 			canvas.style.zIndex = 1;
 
+			// check if interval is runnning
 			if (!intervalStarted) {
 				intervalStarted = true;
 				setInterval(async () => {
+					// check if video and game are runnning
 					if (!video.paused && !video.ended && gameStarted) {
+						// set face API options for detection
 						const detections = await faceapi
 							.detectAllFaces(video, new faceapi.SsdMobilenetv1Options())
 							.withFaceLandmarks()
@@ -101,36 +105,46 @@ async function startSmileDetection() {
 							detections,
 							displaySize
 						);
+						// creat rect to drwan stats inside
 						canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+						// drwan stats
 						faceapi.draw.drawDetections(canvas, resizedDetections);
 						faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
 
+						// check if detection indicates smile
 						if (resizedDetections.length >= 1) {
+							// check if happy expression is higher the allowed
 							if (resizedDetections[0].expressions.happy > 0.4) {
+								// set a smile for this itteration / frame
 								consecutiveSmiles1++;
+
+								// check for smiling lenght and not allready is caught smiling atm
 								if (
 									consecutiveSmiles1 >= MIN_CONSECUTIVE_FRAMES &&
 									!isSmiling1
 								) {
-									smileCounter1++;
+
+									// indicate that is cuaght smiling atm
 									isSmiling1 = true;
+									// reset smilling itteration / frame count to 0
 									consecutiveSmiles1 = 0;
 
-									// Update life
-
+									// Update lives
 									updateLife()
 
 								}
 							} else {
+								// indicate that isn't cuaght smiling atm
 								isSmiling1 = false;
+								// set smilling itteration / frame count to 0
 								consecutiveSmiles1 = 0;
 							}
 						}
 					}
 
 
-				}, 100);
-				intervalStarted = true;
+				}, 100); // start interval
+				intervalStarted = true; // set it initial to true
 			}
 		});
 	} catch (error) {
